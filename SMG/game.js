3c8fb1fee8f777f85fd8b9d4578db0a69c8a82bc
Fredrik Palm <frepal094@gmail.com>
2019-01-26 23:27:55 -0800

    smg 2

--- SMG/game.js
+++ SMG/game.js
@@ -18,6 +18,11 @@ function toUpper(s)
 	return s[0].toUpperCase() + s.substr(1);
 }
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
 function swap(a, b)
 {
 	var c = a;
@@ -139,7 +144,7 @@ function jumpTo(e)
 	 - surrounded by zombies
 	 - Isaac's fashioned artillery to beat the shit out of the zombies
 
-	- 
+	- Together 
 
 
 	Event("You're in your bedroom", [new Choice("Get dressed", function(){
@@ -166,16 +171,100 @@ function showTextThenGoTo(text, event, time)
 	}, time);
 }
 
-var events = [
-	new Event("You wake up. It's 7am. It's quiet\nYou still feel tired as if you haven't slept", 
-	[new Choice("Snooze", function(){ jumpTo(1); }), new Choice("Get up", function(){ jumpTo(2); })]),
-	new Event("You hit snooze, roll over to the other side and close your eyes", 
-	[new Choice("Snooze", function(){ showTextThenGoTo("Test", 1); }), new Choice("Get up", function(){ jumpTo(2); })]),
-	new Event("You step out of the bed. You're in your room", 
-	[new Choice("Get dressed", function(){ jumpTo(3); }), new Choice("Check your phone", function(){ showTextThenGoTo("Huh, no messages.", 2); })]),	
-	new Event("You're looking at the dresser", 
-	[new Choice("Get dressed", function(){ jumpTo(4); }), new Choice("Get up", function(){ jumpTo(2); })])	
-];
+function makeJumpTo(e)
+{
+	return function() { jumpTo(e); };
+}
+
+function makeChoice(text, fn, c)
+{
+	return new Choice(text, fn, c);
+}
+
+function jumpChoice(text, e, c)
+{
+	return new Choice(text, makeJumpTo(e), c);
+}
+
+function descChoice(text, description, c)
+{
+	return new Choice(text, function(){ return showEvent(new Event(description)); }, c);
+}
+
+/*
+	Saturday, February 2nd
+
+
+*/
+
+var snoozed = false;
+var clothed = false;
+var phone = false;
+var checkedNooneHome = false;
+var bookOnFloor = true;
+var braOn = false;
+var shirtOn = false;
+var pantsOn = false;
+
+var secretMode = true;
+
+function makeAwake()
+{
+	if(snoozed){
+		return "Just as you feel yourself slipping away, the alarm starts playing again\nIf anything you feel even more tired";
+	}
+	return "You find yourself awake. It's 8am. An alarm is playing\nYou feel as though you've barely slept";
+}
+
+function makeGetup()
+{
+	var s = "You stumble out of bed. You're in your room\nThe house feels oddly quiet as if no one's home\nRemnants of last night's project are on an uncharacteristically messy desk";
+	if(bookOnFloor) s += "\nTo your surprise, a book has fallen off the shelf";
+	return s;
+}
+
+var events = {
+	awake: dEvent(makeAwake, [jumpChoice("Get up", "getup"), jumpChoice("Snooze", "snooze")]),
+	snooze: nEvent("You hit snooze, roll over to the other side and close your eyes.. and fade", "awake").effect(function(){ snoozed = true; }),
+	
+	phone: dEvent("Disabling the alarm, you notice something is odd with your phone"),
+	
+	//room
+	getup: dEvent(makeGetup, 
+	[jumpChoice("Get dressed", "dresser", function(){ return !clothed; }), jumpChoice("Look at the desk", "desk"), 
+	 jumpChoice("Have a look in the Ollivander's box", "wands", function(){ return secretMode && !bookOnFloor; }), jumpChoice("Pick up the book", "book", function(){ return bookOnFloor; }), jumpChoice("Check your phone", "phone"), jumpChoice("Pet the giraffe", "giraffe"), jumpChoice("Check if anyone's home", "house", function(){ return clothed && phone; })]),	
+	desk: dEvent("You hardly remember what you were doing. There's all sorts of paper and colors and strings\nSigns of a glorious night"),
+	giraffe: dEvent("Heyyyy giraffe! How ya doing?\nReaching up to some tasty leaves?\nNot missing the savannah too much?\nNot guarding a secret lair of any kind..", [ jumpChoice("That's a happy giraffe <i>pat pat</i>", "getup"), jumpChoice("<i>Pat pat</i>... wait what?", "getup")]),
+	wands: dEvent("You're not sure what you're doing, but your mind is leading you to the Ollivander's box\nIt opens, revealing Rupert and his friends inside", [ jumpChoice("Good to know they're safe. Close the box", "getup"), jumpChoice("Let's see how Rupert's doing", "rupert"), , jumpChoice("I've always wondered what the merman got up to", "merman")]),
+	book: dEvent("You're holding a Forensic Art book\nA skull glares at you, reminding you how cool the class in Texas was\nIt feels like an age ago", 
+	[jumpChoice("Sigh. Put it back on the shelf", "shelf"), jumpChoice("Read for a bit", "reading")]),
+	shelf: nEvent("You put the book on the shelf, right next to the Ollivander's wand box", "getup").effect(function(){ bookOnFloor = false }),
+	phone: dEvent("You pick up your OnePlus, never fully over the loss of the once great Blackberry, ready to see last night's unread messages but there are none\n", []).effect(function(){ phone = true; }),
+	dresser: dEvent("You go to the mirror to get dressed\nA bronze haired goddess looks back at you", 
+	[descChoice("Put on a bra", "You pick one of the less sucky bras and strap on, one boob at a time", function(){ return !braOn && !shirtOn; }).effect(function(){ braOn = true; }), descChoice("Put on a t-shirt", "Time to make America emo again.. again", function(){ return !shirtOn; }).effect(function(){ shirtOn = true; }), descChoice("Put on pants", "Squuuuuueeeeze. Damn is that a fine butt", function(){ return !pantsOn; }).effect(function(){ pantsOn = true; }), jumpChoice("Looking good", "clothed", function(){ return pantsOn && shirtOn; })]).effect(function(){ clothed = true; }),
+	clothed: nEvent(function(){ if(braOn) return "Looking sweet"; return "Looking sweet, and who needs a bra anyway"}, "getup"),
+	
+	//house
+	house: dEvent("You head out of your room. Your parent's door is open and no one's there\nKelli's room is vacant aside from the bloodlusting Raskolnikov\nNo one's to be seen in the main area. It's very quiet\nPraaaaaise", 
+	[jumpChoice("Go outside", "reccenter")]),
+	
+	//Kelli
+	reccenter: dEvent("Approaching the Rec Center, you see the harrowing sight of walkers roaming the building\nThe thought that Kelli could be one of them is too awful to even consider. She must be alive\nYou can either rush in through the front, or sneak around and find a back entrance", 
+	[jumpChoice("Enter the Rec Center", "reccenter2")]),
+	reccenter2: dEvent("You hear glorious sound of a laugh that could only belong to Kelli\nWhere is she? She must be close", 
+	[jumpChoice("Follow the laugh", "reccenter3")]),
+	reccenter3: dEvent("<i>Ooooooly shit</i>\nYou can't believe what you're seeing\nThe ice rink is covered in blood\nA figure is skating around with a baseball bat in hand, acrobatically dancing around dodging and beating the crap out zombies\n<i>Eat shit you fuuuckers!</i>", [ jumpChoice("<i>KELLIIIIIII</i>", "kelli")]),
+
+
+
+	/*
+		Kelli at the Rec Center
+
+
+	*/
+
+
+};
 
 $(document).ready(function(){
 	setInterval(tick, 10);
@@ -187,7 +276,7 @@ $(document).ready(function(){
 	  }
 	);
 
-	showEvent(events[0]);
+	showEvent(events.awake);
 });
 
 function buildCharacterUI(character)
@@ -262,31 +351,20 @@ var lastEvent;
 
 function showEvent(event)
 {
+	if(!event) { showEvent(lastEvent); return; }
 	console.log("starting event");
 	paused = true;
 
-	$("#EventContent").html(event.content);
+	var content = (event.contentFn) ? formatText(event.contentFn()) : event.content;
+	$("#EventContent").html(content);
 	$("#EventContent").fadeTo(0,0).fadeTo(700, 1.0);
 	$("#EventChoices").fadeTo(0,0).fadeTo(1200, 1.0);
 	var choices = $("#EventChoices").empty();
 
-	var last = lastEvent;
-	if(event.choices.length == 0){
-		setTimeout(function(){
-			endEvent();
-			$("#EventChoices").fadeTo(0,0);
-			setTimeout(function(){
-				showEvent(last);
-			}, 1100);
-		}, 1500);
-		lastEvent = event;
-		return;
-	}
-
 	$.each(event.choices, function(i, choice){
+		if(choice.condition && !choice.condition()) return true;
 		if(choice.text == "") console.log("choice text is empty for ", event);
 		var div = $('<div class="event choice">'+choice.text+'</div>').click(function(e){ 
-			var effect = event.choices[i].effect;
 			var ch = this;
 			$("#EventChoices .choice").each(function(i, t)
 			{
@@ -300,12 +378,30 @@ function showEvent(event)
 			//$(this).fadeTo(2.0, 0.00);
 			endEvent();
 			setTimeout(function(){
-				if(effect) effect();
+				if(choice.effects){ 
+					$.each(choice.effects, function(i, effect){ effect(); });
+				}
 			}, 1500);
 		});
 
 		choices.append(div);							
 	});
+
+	var last = (event.next) ? event.next : lastEvent;
+	if(last && last.length) last = events[last];
+	if(choices.children().length == 0){
+		setTimeout(function(){
+			endEvent();
+			$("#EventChoices").fadeTo(0,0);
+			setTimeout(function(){
+				showEvent(last);
+			}, 1100);
+		}, 1500);
+	}
+
+	if(event.effects){ 
+		$.each(event.effects, function(i, effect){ effect();});
+	}
 
 	lastEvent = event;
 }
@@ -331,19 +427,50 @@ function endEvent()
 
 function formatText(text)
 {
-	text = text.replace('\n', "<p></p>");
+	text = text.replaceAll('\n', "<p></p>");
 	return text.replace(/\*([a-zA-Z]*)\*/gi, "<span class='note'>$1</span>");
+}
+
+function nEvent(content, next)
+{
+	var n = new Event(content, []);
+	n.next = next;
+	return n;
+}
+
+function dEvent(content, choices)
+{
+	return new Event(content, choices);
 }
 
 function Event(content, choices)
 {
-	this.content = formatText(content);
-	this.choices = choices;
+	if($.isFunction(content)) this.contentFn = content; 
+	else this.content = formatText(content);
+
+	this.choices = (choices) ? choices : [];
+
+	this.effects = [];
+
+	var t = this;
+	this.effect = function(fn){
+		t.effects.push(fn);
+		return t;
+	}
 }
 
-function Choice(text, effect)
+function Choice(text, effect, condition)
 {
 	this.text = formatText(text);
-	this.effect = effect;
+	this.condition = condition;
+
+	this.effects = [];
+	if(effect) this.effects.push(effect);
+
+	var t = this;
+	this.effect = function(fn){
+		t.effects.push(fn);
+		return t;
+	}
 }
 
